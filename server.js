73d905016ee775749431e5e8175d12827af1bb0c
PtPrashantTripathi <PtPrashantTripathi@outlook.com>
2023-08-23 02:08:06 +0530

    Formatted

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 app.use((req, res, next) => {
-  res.status(404).send('<h1> Page not found </h1>');
+  res.status(404).send("<h1> Page not found </h1>");
 });
 const PORT = process.env.PORT || 80;
 const server = app.listen(PORT, () => {
   console.log(
-    '\x1b[42m',
+    "\x1b[42m",
     `listening on port ${server.address().port}...`,
-    '\x1b[5m',
+    "\x1b[5m",
   );
 });
