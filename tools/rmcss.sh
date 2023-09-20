5322b130b0f6f0bdf9dc1d042b4681324c30257a
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2023-09-20 23:36:24 +0530

    Delete tools directory

--- tools/rmcss.sh
+++
@@ -1,4 +0,0 @@
-for filename in $(find . -name "*.css"); do
-  echo "$filename"
-  purgecss --css "$filename" --content index.html --output "$filename"
-done
--- tools/rmjs.sh
+++
@@ -1,4 +0,0 @@
-for filename in $(find . -name "*.js"); do
-  echo "$filename"
-  google-closure-compiler --js "$filename" --js_output_file "$filename"
-done
